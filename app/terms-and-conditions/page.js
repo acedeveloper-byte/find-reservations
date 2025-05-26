ea0d6dfeb1604b55c288740d71092234430e2e81
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-26 13:35:29 +0530

    added changes

--- app/terms-and-conditions/page.js
+++ app/terms-and-conditions/page.js
@@ -1,72 +1,136 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const page = () => {
-  return (
+	return (
+		<>
+			<Header />
+			<h1 className='text-center my-4'>Terms and conditions</h1>
+			<hr />
 
-    <>
-    <Header/>
-      <h1 className='text-center my-4'>Terms and conditions</h1>
-      <hr/>
-    
-   <Container>
-    <p>Welcome to Find-Reservations (referred to as &ldquo;we,&rdquo; &ldquo;our,&rdquo; &ldquo;us,&rdquo; or the &ldquo;Site&rdquo;), available at find-reservations.com. By using Find-Reservations, the user agrees to comply with and be legally bound by these described Terms and Conditions. Please go through these Terms &amp; Conditions carefully before using our services.</p>
+			<Container>
+				<p>
+					Welcome to Find-Reservations (referred to as &ldquo;we,&rdquo;
+					&ldquo;our,&rdquo; &ldquo;us,&rdquo; or the &ldquo;Site&rdquo;),
+					available at find-reservations.com. By using Find-Reservations, the
+					user agrees to comply with and be legally bound by these described
+					Terms and Conditions. Please go through these Terms &amp; Conditions
+					carefully before using our services.
+				</p>
 
-<h3>1. Acceptance of Terms</h3>
-<p>By accessing or using Find-Reservations to search, compare, or book travel services, you agree to be bound by these Terms and Conditions and our Privacy Policy. If you do not accept these terms, you are not authorized to use this Site.</p>
+				<h3>1. Acceptance of Terms</h3>
+				<p>
+					By accessing or using Find-Reservations to search, compare, or book
+					travel services, you agree to be bound by these Terms and Conditions
+					and our Privacy Policy. If you do not accept these terms, you are not
+					authorized to use this Site.
+				</p>
 
-<h3>2. About Find-Reservations</h3>
-<p>Find-Reservations is a third-party travel platform. We do not own, operate, or control the airlines listed on our site. We are just a third-party travel provider that helps you connect with your favorite airline at low fares.</p>
+				<h3>2. About Find-Reservations</h3>
+				<p>
+					Find-Reservations is a third-party travel platform. We do not own,
+					operate, or control the airlines listed on our site. We are just a
+					third-party travel provider that helps you connect with your favorite
+					airline at low fares.
+				</p>
 
-<h3>3. Third-Party Bookings</h3>
-<p>All bookings are made directly with third-party providers and are subject to their terms and conditions, including but not limited to payment, cancellation, change, and refund policies.</p>
+				<h3>3. Third-Party Bookings</h3>
+				<p>
+					All bookings are made directly with third-party providers and are
+					subject to their terms and conditions, including but not limited to
+					payment, cancellation, change, and refund policies.
+				</p>
 
-<p>Find-Reservations is not responsible for any booking, fulfillment, delay, cancellation, or quality of service provided by third-party vendors.</p>
+				<p>
+					Find-Reservations is not responsible for any booking, fulfillment,
+					delay, cancellation, or quality of service provided by third-party
+					vendors.
+				</p>
 
-<p>Any disputes or claims must be directed to the provider with whom you completed the booking.</p>
+				<p>
+					Any disputes or claims must be directed to the provider with whom you
+					completed the booking.
+				</p>
 
-<h3>4. User Responsibilities</h3>
-<p>By using our site, you agree to:</p>
+				<h3>4. User Responsibilities</h3>
+				<p>By using our site, you agree to:</p>
 
-<ul>
-<li >Provide accurate and current information.</li>
-<li >Use the site lawfully and not for any fraudulent or unauthorized purpose.</li>
-<li >Be responsible for reviewing the terms, conditions, and policies of any third-party service provider before booking.</li>
-</ul>
+				<ul>
+					<li>Provide accurate and current information.</li>
+					<li>
+						Use the site lawfully and not for any fraudulent or unauthorized
+						purpose.
+					</li>
+					<li>
+						Be responsible for reviewing the terms, conditions, and policies of
+						any third-party service provider before booking.
+					</li>
+				</ul>
 
-<h3>5. Intellectual Property</h3>
-<p>All content on Find-Reservations, including but not limited to logos, graphics, text, icons, and software, is the property of Find-Reservations or its licensors and is protected by applicable trademark, copyright, and other intellectual property laws.</p>
+				<h3>5. Intellectual Property</h3>
+				<p>
+					All content on Find-Reservations, including but not limited to logos,
+					graphics, text, icons, and software, is the property of
+					Find-Reservations or its licensors and is protected by applicable
+					trademark, copyright, and other intellectual property laws.
+				</p>
 
-<h3>6. Limitation of Liability</h3>
-<p>Subject to the applicable law:</p>
+				<h3>6. Limitation of Liability</h3>
+				<p>Subject to the applicable law:</p>
 
-<ul>
-<li >We make no warranties or representations regarding the availability, accuracy, or reliability of the site or its content.</li>
-<li >We shall not be held responsible for any direct, indirect, consequential, incidental, or punitive damages resulting from your use of the site or from third-party service failures.</li>
-<li >We are not responsible for the acts or omissions of any travel provider.</li>
-</ul>
+				<ul>
+					<li>
+						We make no warranties or representations regarding the availability,
+						accuracy, or reliability of the site or its content.
+					</li>
+					<li>
+						We shall not be held responsible for any direct, indirect,
+						consequential, incidental, or punitive damages resulting from your
+						use of the site or from third-party service failures.
+					</li>
+					<li>
+						We are not responsible for the acts or omissions of any travel
+						provider.
+					</li>
+				</ul>
 
-<h3>7. Modifications to the Site or Terms</h3>
-<p>We may update or modify these Terms of Service whenever needed without prior notice. We will post all the modifications on this page, and they will be effective immediately. If you keep using the Site after we make changes, it means you agree to the new Terms.</p>
+				<h3>7. Modifications to the Site or Terms</h3>
+				<p>
+					We may update or modify these Terms of Service whenever needed without
+					prior notice. We will post all the modifications on this page, and
+					they will be effective immediately. If you keep using the Site after
+					we make changes, it means you agree to the new Terms.
+				</p>
 
-<h3>8. Privacy Policy</h3>
-<p>Your use of the site is also governed by our Privacy Policy. This policy describes how we gather, use, and safeguard your personal information.</p>
+				<h3>8. Privacy Policy</h3>
+				<p>
+					Your use of the site is also governed by our Privacy Policy. This
+					policy describes how we gather, use, and safeguard your personal
+					information.
+				</p>
 
-<h3>9. Governing Law</h3>
-<p>Your use of the Site and these Terms will be governed by the laws of the United States, without reference to its conflict of law provisions.</p>
+				<h3>9. Governing Law</h3>
+				<p>
+					Your use of the Site and these Terms will be governed by the laws of
+					the United States, without reference to its conflict of law
+					provisions.
+				</p>
 
-<h3>10. Contact Information</h3>
-<p>If you have any queries about these Terms and Conditions, please contact us at:</p>
+				<h3>10. Contact Information</h3>
+				<p>
+					If you have any queries about these Terms and Conditions, please
+					contact us at:
+				</p>
 
-<p>Find-Reservations</p>
-<p>Email: support@find-reservations.com</p>
-   </Container>
+				<p>Find-Reservations</p>
+				<p>Email: support@find-reservations.com</p>
+			</Container>
 
-<Footer/>
-    </>
-  )
-}
+			<Footer />
+		</>
+	);
+};
 
-export default page
+export default page;
